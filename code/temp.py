98574b7302083bed8951a6c51fc9677c9158b6ec
jackstark <jackstark@qq.com>
2019-06-17 14:25:04 +0800

    Update temp.py

--- code/temp.py
+++ code/temp.py
@@ -1,14 +1,13 @@
-def fibo(n):
-    first_number = 0
-    second_number = 1
+def iou(x1,y1,w1,h1,x2,y2,w2,y2):
+    up = min(y1 + h1 / 2, y2 + h2 / 2)
+    down = max(y1 - h1 / 2, y2 - h2 / 2)
+    left = max(x1 - w1 / 2, x2 - w2 / 2)
+    right = min(x1 + w1 / 2, x2 + w2 / 2)
 
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    for _ in range(n):
-        first_number, second_number = second_number, first_number+second_number
-    return first_number
+    intersetion = abs((up - down) * (right - left))
+    union = w1 * h1 + w2 * h2 - intersetion
+    return intersetion / union
+    
 
 if __name__ == '__main__':
     print(fibo(5))
