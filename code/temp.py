d3999a05dd931a4d5fc001050d2b9c5653847f27
jackstark <jackstark@qq.com>
2019-07-18 15:11:06 +0800

    矩阵中的路径

--- code/temp.py
+++ code/temp.py
@@ -1,37 +1,37 @@
 # -*- coding:utf-8 -*-
 class Solution:
     def hasPath(self, matrix, rows, cols, path):
+        matrix = list(matrix)
+        path = list(path)
+        cur_path = 0
+        flags = [0] * len(matrix)
+
         for row in range(rows):
             for col in range(cols):
-                if matrix[row * cols + col] == path[0]:
-                    if self.find(list(matrix), rows, cols, path[1:], row, col):
-                        return True
+                if self.find(matrix, rows, cols, row, col, path, cur_path, flags):
+                    return True
         return False
 
-    def find(self, matrix, rows, cols, path, row, col):
-        if not path:
-            return True
-        print(row, col, matrix[row * cols + col])
-        matrix[row * cols + col] = '0'
-        for i in range(rows):
-            for j in range(cols):
-                print(matrix[i * cols + j], end=" ")
-            print()
-        print()
-        if col + 1 <= cols - 1 and matrix[row * cols + (col + 1)] == path[0]:
-            return self.find(matrix, rows, cols, path[1:], row, col + 1)
-        elif col - 1 >= 0 and matrix[row * cols + (col - 1)] == path[0]:
-            return self.find(matrix, rows, cols, path[1:], row, col - 1)
-        elif row + 1 <= rows - 1 and matrix[(row + 1) * cols + col] == path[0]:
-            return self.find(matrix, rows, cols, path[1:], row + 1, col)
-        elif row - 1 >= 0 and matrix[(row - 1) * cols + col] == path[0]:
-            return self.find(matrix, rows, cols, path[1:], row - 1, col)
-        else:
+    def find(self, matrix, rows, cols, row, col, path, cur_path, flags):
+        index = row * cols + col
+
+        if col < 0 or col > cols -1 or row < 0 or row > rows - 1 or flags[index]  == 1 or matrix[index] != path[cur_path]:
             return False
+        if cur_path == len(path) - 1:
+            return True
+
+        flags[index] = 1
+        if self.find(matrix, rows, cols, row + 1, col, path, cur_path + 1,flags) or \
+            self.find(matrix, rows, cols, row - 1, col, path, cur_path + 1, flags) or \
+            self.find(matrix, rows, cols, row, col + 1, path, cur_path + 1, flags) or \
+            self.find(matrix, rows, cols, row, col - 1, path, cur_path + 1, flags):
+            return True
+        flags[index] = 0
+        return False
 
 solution = Solution()
 
-#print(solution.hasPath("ABCESFCSADEE",3,4,"SEC"))
-#print(solution.hasPath("ABCEHJIGSFCSLOPQADEEMNOEADIDEJFMVCEIFGGS",5,8,"SGGFIECVAASABCEHJIGQEM"))
-#print(solution.hasPath("ABTGCFCSJDEH",3,4,"BFCE"))
+print(solution.hasPath("ABCESFCSADEE",3,4,"SEC"))
+print(solution.hasPath("ABCEHJIGSFCSLOPQADEEMNOEADIDEJFMVCEIFGGS",5,8,"SGGFIECVAASABCEHJIGQEM"))
+print(solution.hasPath("ABTGCFCSJDEH",3,4,"BFCE"))
 print(solution.hasPath("ABTGCFCSJDEH",3,4,"BFCJ"))
