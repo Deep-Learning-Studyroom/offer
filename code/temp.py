56b605b7f51af35484a47e08fb9ab8ce857d24dd
Jack Stark <jackstark@qq.com>
2019-07-19 12:36:25 +0800

    打印1到最大的n位数

--- code/temp.py
+++ code/temp.py
@@ -1,13 +1,47 @@
-# -*- coding:utf-8 -*-
-class Solution:
-    def Power(self, base, exponent):
-        # write code here
-        if base == 0:
-            return 0
-        if exponent == 1:
-            return base
+def my_print(n):
+    """python语言的优点，数字没有上限，可以直接打印"""
+    max_val = 1
+    for _ in range(n):
+        max_val *= 10
+    for i in range(1, max_val):
+        print(i)
+    return
 
-        if exponent % 2 == 0:
-            return self.Power(base, exponent / 2) * self.Power(base, exponent / 2)
-        if exponent % 2 == 1:
-            return self.Power(base, (exponent - 1) / 2) * self.Power(base, (exponent - 1) / 2) * base
+
+def my_print2(n):
+    """
+    数字的全排列，用递归的方法实现
+    """
+    if n <= 0:
+        return
+
+    num = []
+    #num_last = num
+    #for i in range(10):
+    #    num = [str(i)] + num_last
+    #    print_recursive(n, num)
+    print_recursive(n, num)
+def print_recursive(n,num):
+    if len(num) > n :
+        return
+    if len(num) == n:
+        print_number(num)
+
+    if len(num) <= n-1:
+        num_last = num
+        for i in range(10):
+            num = num_last + [str(i)]
+            print_recursive(n, num)
+
+def print_number(num):
+    is_begining0 = True
+    n = len(num)
+    for i in range(n):
+        if is_begining0 and num[i] != '0':
+            is_begining0 = False
+        if not is_begining0:
+            print(num[i], end="")
+    print()
+
+#my_print(3)
+my_print2(3)
--- code/剑指offer.md
+++ code/剑指offer.md
@@ -882,6 +882,71 @@ class Solution:
                 return 1. / (self.Power(base, (exponent-1)/2) * self.Power(base, (exponent-1)/2) * base)
 ```
 
+# 面试题12：打印1到最大的n位数
+
+解法一：适合Python，不适合C++（大数溢出问题）。先算出最大范围，然后依次打印。不是面试官满意的算法。
+
+解法三：递归。面试时用这个最好。**注意：打印函数要单独写一个，因为数字开头的0不应该被打印出来。**  
+
+解法二：使用字符串模拟加法。但是代码太长 ，不推荐。
+
+```python
+def my_print(n):
+    """python语言的优点，数字没有上限，可以直接打印"""
+    max_val = 1
+    for _ in range(n):
+        max_val *= 10
+    for i in range(1, max_val):
+        print(i)
+    return
+
+
+def my_print2(n):
+    """
+    数字的全排列，用递归的方法实现
+    """
+    if n <= 0:
+        return
+
+    num = []
+    #num_last = num
+    #for i in range(10):
+    #    num = [str(i)] + num_last
+    #    print_recursive(n, num)
+    print_recursive(n, num)
+def print_recursive(n,num):
+    if len(num) > n :
+        return
+    if len(num) == n:
+        print_number(num)
+
+    if len(num) <= n-1:
+        num_last = num
+        for i in range(10):
+            num = num_last + [str(i)]
+            print_recursive(n, num)
+
+def print_number(num):
+    is_begining0 = True
+    n = len(num)
+    for i in range(n):
+        if is_begining0 and num[i] != '0':
+            is_begining0 = False
+        if not is_begining0:
+            print(num[i], end="")
+    print()
+
+my_print(3)
+my_print2(3)
+```
+
+
+
+
+
+
+
+# 面试太13：
 
 # 面试题32：从上到下打印二叉树
 
