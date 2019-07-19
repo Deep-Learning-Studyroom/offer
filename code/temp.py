dcf9d7fde76b53cb6d50c5198121a88b9b1dca2d
jackstark <jackstark@qq.com>
2019-07-19 21:03:30 +0800

    中值滤波和均值滤波

--- code/temp.py
+++ code/temp.py
@@ -1,16 +1,20 @@
-class Solution:
-    def deleteDuplication(self, pHead):
-        # write code here
-        if pHead is None or pHead.next is None:
-            return pHead
-        head1 = pHead.next
-        if pHead.val != head1.val:
-            pHead.next = self.deleteDuplication(pHead.next)
-        else:
-            while pHead.val == head1.val and head1.next is not None:
-                head1 = head1.next
-            if pHead.val != head1.val:
-                pHead = self.deleteDuplication(head1)
-            else:
-                return None
-        return pHead
+import numpy as np
+def mean_filter(image, kernel_size):
+    """
+    :param image: 图像数据 numpy array
+    :kernel_size: 滤波器大小，奇数
+    :return: 滤波后的图像
+    """
+
+    k = (kernel_size - 1) / 2
+    height, width = image.shape[0], image.shape[1]
+    for row in range(1, height - 1):
+        for col in range (1, width - 1):
+            # get the area to be filtered
+            filtered_area = image[row - k: row + k + 1, col - k: col + k + 1]
+            filtered_area = filtered_area.flatten()
+            filtered_area = np.sort(filtered_area)
+            res = filtered_area[(kernel_size ** 2 - 1) // 2]
+            image[row, col] = res
+
+    return image
--- code/重要机器学习算法代码/中值滤波.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def filter():
-    """
-
-    :return:
-    """
