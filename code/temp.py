f483250d0a960a00a4df70f79835a82ec8421663
jackstark <jackstark@qq.com>
2019-07-19 15:25:56 +0800

    大数相加和大数相减

--- code/temp.py
+++ code/temp.py
@@ -1,47 +1,100 @@
-def my_print(n):
-    """python语言的优点，数字没有上限，可以直接打印"""
-    max_val = 1
-    for _ in range(n):
-        max_val *= 10
-    for i in range(1, max_val):
-        print(i)
-    return
+def big_number_sum(s1, s2):
+    """首先写出两个整数相加和相减的函数，然后根据两个数的正负号分四种类别计算"""
+    if s1[0] != '-' and s2[0] != '-':
+        big_positive_number_sum(s1, s2)
+    elif s1[0] == '-' and s2[0] != '-':
+        s1 = s1[1:]
+        big_positive_number_minus(s2, s1)
+    elif s1[0] != '-' and s2[0] == '-':
+        s2 = s2[1:]
+        big_positive_number_minus(s1, s2)
+    else:
+        s1 = s1[1:]
+        s2 = s2[1:]
+        print('-', end="")
+        big_positive_number_sum(s1, s2)
 
+def big_positive_number_sum(s1, s2):
+    """两个大正数相加，首先对齐(补0)，然后按加法计算规则计算"""
+    L1=[0]
+    L2=[0]
+    for i in range(0,len(s1)):
+        L1.append(int(s1[i]))
+    for i in range(0,len(s2)):
+        L2.append(int(s2[i]))
 
-def my_print2(n):
-    """
-    数字的全排列，用递归的方法实现
-    """
-    if n <= 0:
-        return
+    if(len(s1)>len(s2)):
+        for i in range(len(s1)-len(s2)):
+            L2.reverse()
+            L2.append(0)
+            L2.reverse()
+    elif(len(s1)<=len(s2)):
+        for i in range(len(s2)-len(s1)):
+            L1.reverse()
+            L1.append(0)
+            L1.reverse()
 
-    num = []
-    #num_last = num
-    #for i in range(10):
-    #    num = [str(i)] + num_last
-    #    print_recursive(n, num)
-    print_recursive(n, num)
-def print_recursive(n,num):
-    if len(num) > n :
-        return
-    if len(num) == n:
-        print_number(num)
+    for i in range(len(L1)):
+            L1[i]=L1[i]+L2[i]
+    A=B=len(L1)-1
 
-    if len(num) <= n-1:
-        num_last = num
-        for i in range(10):
-            num = num_last + [str(i)]
-            print_recursive(n, num)
-
-def print_number(num):
-    is_begining0 = True
-    n = len(num)
-    for i in range(n):
-        if is_begining0 and num[i] != '0':
-            is_begining0 = False
-        if not is_begining0:
-            print(num[i], end="")
+    while A>0:
+        if((L1[A])/10)>=1:
+            L1[A]=L1[A]%10
+            L1[A-1]=L1[A-1]+1
+        A-=1
+    if L1[0]==0:
+        for i in range(1,B+1):
+            print(L1[i],end='')
+    elif L1[0]!=0:
+        for i in range(B+1):
+            print(L1[i],end='')
     print()
 
-#my_print(3)
-my_print2(3)
+def big_positive_number_minus(s1, s2):
+    """减法：注意一点，如果s1减去s2得到的第一位的数字是-1，那么说明s2更大，先打印一个负号，然后调用big_positive_number_minus(s2, s1)"""
+    L1=[0]
+    L2=[0]
+    for i in range(0,len(s1)):
+        L1.append(int(s1[i]))
+    for i in range(0,len(s2)):
+        L2.append(int(s2[i]))
+
+    if(len(s1)>len(s2)):
+        for i in range(len(s1)-len(s2)):
+            L2.reverse()
+            L2.append(0)
+            L2.reverse()
+    elif(len(s1)<=len(s2)):
+        for i in range(len(s2)-len(s1)):
+            L1.reverse()
+            L1.append(0)
+            L1.reverse()
+
+    for i in range(len(L1)):
+            L1[i]=L1[i] - L2[i]
+    A=B=len(L1)-1
+
+    while A>0:
+        if L1[A] < 0:
+            L1[A]=L1[A] + 10
+            L1[A-1]=L1[A-1] - 1
+        A-=1
+    if L1[0]==0:
+        for i in range(1, B+1):
+            print(L1[i],end='')
+            if i == B:
+                print()
+    elif L1[0]!=0:
+        print('-', end="")
+        big_positive_number_minus(s2, s1)
+
+big_number_sum('1323479817', '1372987318423498414')
+big_number_sum('1323479817', '-1372987318423498414')
+big_number_sum('-1323479817', '1372987318423498414')
+big_number_sum('-1323479817', '-1372987318423498414')
+print()
+print(1323479817 + 1372987318423498414)
+print(1323479817 - 1372987318423498414)
+print(-1323479817 + 1372987318423498414)
+print(-1323479817 - 1372987318423498414)
--- code/剑指offer.md
+++ code/剑指offer.md
@@ -884,6 +884,8 @@ class Solution:
 
 # 面试题12：打印1到最大的n位数
 
+## 打印1到最大的n位数
+
 解法一：适合Python，不适合C++（大数溢出问题）。先算出最大范围，然后依次打印。不是面试官满意的算法。
 
 解法三：递归。面试时用这个最好。**注意：打印函数要单独写一个，因为数字开头的0不应该被打印出来。**  
@@ -940,7 +942,114 @@ my_print(3)
 my_print2(3)
 ```
 
+## 补充题：大数相加
 
+>定义一个函数，该函数可以实现任意两个整数的加法。
+
+思路：任意两个整数的加法，因此是大数问题，需要用字符串表示或列表表示的加法。另外，还需要考虑负数的情况。
+
+```python
+def big_number_sum(s1, s2):
+    """首先写出两个整数相加和相减的函数，然后根据两个数的正负号分四种类别计算"""
+    if s1[0] != '-' and s2[0] != '-':
+        big_positive_number_sum(s1, s2)
+    elif s1[0] == '-' and s2[0] != '-':
+        s1 = s1[1:]
+        big_positive_number_minus(s2, s1)
+    elif s1[0] != '-' and s2[0] == '-':
+        s2 = s2[1:]
+        big_positive_number_minus(s1, s2)
+    else:
+        s1 = s1[1:]
+        s2 = s2[1:]
+        print('-', end="")
+        big_positive_number_sum(s1, s2)
+
+def big_positive_number_sum(s1, s2):
+    """两个大正数相加，首先对齐(补0)，然后按加法计算规则计算"""
+    L1=[0]
+    L2=[0]
+    for i in range(0,len(s1)):
+        L1.append(int(s1[i]))
+    for i in range(0,len(s2)):
+        L2.append(int(s2[i]))
+
+    if(len(s1)>len(s2)):
+        for i in range(len(s1)-len(s2)):
+            L2.reverse()
+            L2.append(0)
+            L2.reverse()
+    elif(len(s1)<=len(s2)):
+        for i in range(len(s2)-len(s1)):
+            L1.reverse()
+            L1.append(0)
+            L1.reverse()
+
+    for i in range(len(L1)):
+            L1[i]=L1[i]+L2[i]
+    A=B=len(L1)-1
+
+    while A>0:
+        if((L1[A])/10)>=1:
+            L1[A]=L1[A]%10
+            L1[A-1]=L1[A-1]+1
+        A-=1
+    if L1[0]==0:
+        for i in range(1,B+1):
+            print(L1[i],end='')
+    elif L1[0]!=0:
+        for i in range(B+1):
+            print(L1[i],end='')
+    print()
+
+def big_positive_number_minus(s1, s2):
+    """减法：注意一点，如果s1减去s2得到的第一位的数字是-1，那么说明s2更大，先打印一个负号，然后调用big_positive_number_minus(s2, s1)"""
+    L1=[0]
+    L2=[0]
+    for i in range(0,len(s1)):
+        L1.append(int(s1[i]))
+    for i in range(0,len(s2)):
+        L2.append(int(s2[i]))
+
+    if(len(s1)>len(s2)):
+        for i in range(len(s1)-len(s2)):
+            L2.reverse()
+            L2.append(0)
+            L2.reverse()
+    elif(len(s1)<=len(s2)):
+        for i in range(len(s2)-len(s1)):
+            L1.reverse()
+            L1.append(0)
+            L1.reverse()
+
+    for i in range(len(L1)):
+            L1[i]=L1[i] - L2[i]
+    A=B=len(L1)-1
+
+    while A>0:
+        if L1[A] < 0:
+            L1[A]=L1[A] + 10
+            L1[A-1]=L1[A-1] - 1
+        A-=1
+    if L1[0]==0:
+        for i in range(1, B+1):
+            print(L1[i],end='')
+            if i == B:
+                print()
+    elif L1[0]!=0:
+        print('-', end="")
+        big_positive_number_minus(s2, s1)
+
+big_number_sum('1323479817', '1372987318423498414')
+big_number_sum('1323479817', '-1372987318423498414')
+big_number_sum('-1323479817', '1372987318423498414')
+big_number_sum('-1323479817', '-1372987318423498414')
+print()
+print(1323479817 + 1372987318423498414)
+print(1323479817 - 1372987318423498414)
+print(-1323479817 + 1372987318423498414)
+print(-1323479817 - 1372987318423498414)
+```
 
 
 
