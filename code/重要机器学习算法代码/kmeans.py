9989aaa7684c1d9dd0e8457920b0607801eb4203
jackstark <jackstark@qq.com>
2019-07-22 02:23:43 +0800

    Update kmeans.py

--- code/重要机器学习算法代码/kmeans.py
+++ code/重要机器学习算法代码/kmeans.py
@@ -1,71 +1,44 @@
 import numpy as np
 import random
 
-def kmeans(rows, k):
-    """
-    伪代码：
-    function K-Means(输入数据，中心点个数K)
-    获取输入数据的维度Dim和个数N
-    随机生成K个Dim维的点
-    while(算法未收敛)
-        对N个点：计算每个点属于哪一类。
-        对于K个中心点：
-            1，找出所有属于自己这一类的所有数据点
-            2，把自己的坐标修改为这些数据点的中心点坐标
-    end
-    输出结果：
-    end
+def kmeans(data, k):
+    m = len(data)     # 样本个数
+    n = len(data[0])  # 维度
+    cluster_center = np.zeros((k, n))   # k个聚类中心
 
-    算法复杂度：
-        1. 时间复杂度
-        2. 空间复杂度
+    # 选择合适的初始聚类中心
+    j = np.random.randint(m)    # [0, m)
+    cluster_center[0] = data[j][:]   # 在已有数据中随机选择聚类中心，
+                                     # 也可以直接用随机的聚类中心
 
-    """
+    # 聚类
+    cluster = np.zeros(m, dtype=np.int) - 1 # 所有样本尚未聚类
+    cc = np.zeros((k, n))   # 下一轮的聚类中心
+    c_number = np.zeros(k)    # 每个簇样本的数目
 
-
-    # 确定每个点的最大值和最小值，给随机数定个范围
-    ranges = [(min([row[i] for row in rows]), max([row[i] for row in rows]))
-              for i in range(len(rows[0]))]
-
-    # 随机建立k个中心点
-    clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0]
-                 for i in range(len(rows[0]))] for j in range(k)]
-
-    lastmatches = None
-    # 设定循环100次，也可以定义为函数的一个输入
-    for t in range(100):
-        print('Iteration {}'.format(t))
-        bestmatches = [[] for i in range(k)]
-
-        # 在每一行中寻找距离最近的中心点
-        for j, row in enumerate(rows):
-            bestmatch = 0
-            for i in range(k):
-                d = distance(clusters[i], row)
-                if d < distance(clusters[bestmatch], row): bestmatch = i
-            bestmatches[bestmatch].append(j)
-
-            # 如果结果与上一次的相同，则整个过程结束
-        if bestmatches == lastmatches:
-            break
-        lastmatches = bestmatches
-
-        # 将中心点移到其所有成员的平均位置处
+    for times in range(40):
+        for i in range(m):
+            c = nearst(data[i], cluster_center)
+            cluster[i] = c  # 第i个样本归于第c簇
+            c_number[c] += 1
+            cc[c] += data[i]
         for i in range(k):
-            avgs = [0.0] * len(rows[0])
-            if len(bestmatches[i]) > 0:
-                for rowid in bestmatches[i]:
-                    for m in range(len(rows[rowid])):
-                        avgs[m] += rows[rowid][m]
-                for j in range(len(avgs)):
-                    avgs[j] /= len(bestmatches[i])
-                clusters[i] = avgs
+            cluster_center[i] = cc[i] / c_number[i]
+        cc.flat = 0
+        c_number.flat = 0
+    return cluster
 
-    return bestmatches
+def nearst(data, cluster_center):
+    nearst_center_index = 0
+    dis = np.sum((cluster_center[0] - data) ** 2)
+    for index, center in enumerate(cluster_center):
+        dis_temp = np.sum((center - data) ** 2)
+        if dis_temp < dis:
+            nearst_center_index = index
+            dis = dis_temp
+    return nearst_center_index
 
-def distance(array1, array2):
-    # 判断两个数据的维度是否一致
-    if array1.shape != array2.shape:
-        raise ValueError("输入数据维度不一致")
 
-    return np.sqrt((array1 - array2) ** 2)
+
+
+
