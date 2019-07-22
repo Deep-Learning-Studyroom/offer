8961abdfb321c38debbe3056d3f2184f8fd3d11c
jackstark <jackstark@qq.com>
2019-07-22 23:44:32 +0800

    Update temp.py

--- code/temp.py
+++ code/temp.py
@@ -1,20 +1,49 @@
-import numpy as np
-def mean_filter(image, kernel_size):
-    """
-    :param image: 图像数据 numpy array
-    :kernel_size: 滤波器大小，奇数
-    :return: 滤波后的图像
-    """
+# -*- coding:utf-8 -*-
+class Solution:
+    # s字符串
+    def isNumeric(self, s):
+        # write code here
+        if s is None:
+            return False
+        flag, s = self.scanInteger(s)
 
-    k = (kernel_size - 1) / 2
-    height, width = image.shape[0], image.shape[1]
-    for row in range(1, height - 1):
-        for col in range (1, width - 1):
-            # get the area to be filtered
-            filtered_area = image[row - k: row + k + 1, col - k: col + k + 1]
-            filtered_area = filtered_area.flatten()
-            filtered_area = np.sort(filtered_area)
-            res = filtered_area[(kernel_size ** 2 - 1) // 2]
-            image[row, col] = res
+        if s is not None and s[0] == '.':
+            if len(s) > 1:
+                s = s[1:]
+            else:
+                s = None
+            flag1, s = self.scanUnsignedInteger(s)
+            flag = flag or flag1
 
-    return image
+        if s is not None and (s[0] == 'e' or s[0] == 'E'):
+            if len(s) > 1:
+                s = s[1:]
+            else:
+                s = None
+            flag2, s = self.scanInteger(s)
+            flag = flag and flag2
+
+        return flag and s is None
+
+    def scanUnsignedInteger(self, s):
+        flag = False
+        while s is not None and '0' <= s[0] <= '9':
+            if len(s) > 1:
+                s = s[1:]
+            else:
+                s = None
+            flag = True
+        return flag, s
+
+    def scanInteger(self, s):
+        if s is not None and (s[0] == '+' or s[0] == '-'):
+            if len(s) > 1:
+                s = s[1:]
+            else:
+                s = None
+        return self.scanUnsignedInteger(s)
+
+
+print(Solution().isNumeric("12."))
+print(Solution().isNumeric("12.e"))
+print(Solution().isNumeric("12.1e-10"))
